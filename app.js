99b80df12ac48d92677286624830116104786731
mairan-anjum <mairananjum@gmail.com>
2023-04-05 23:35:16 +0500

    now changes

---
+++ app.js
@@ -0,0 +1,38 @@
+/*document.write({ name: "Johan" });
+alert({ name: "Johan" });
+console.log({ name: "Johan" }); */
+
+// Statment & Comment 1:04:17
+// I woke up early him today.
+// statements - sets of instructions
+// comment shortcut command Ctrl + L
+
+// this is the console statment
+/*console.log("Hey How are you");
+console.log("hi i am john");
+console.log("what are you doing there");
+console.log("nothing");*/
+
+// Variable 1:16:20
+let name = "JOHAN SHRIMP TACO V";
+document.write(name);
+console.log(name);
+// some code here
+console.log(name);
+// some code here
+console.log(name);
+// some code here
+console.log(name);
+// some code here
+console.log(name);
+// some code here
+console.log(name);
+// some code here
+// Assign value
+let address, zip, state;
+address = "101 main branch";
+zip = "53700";
+state = "lahore";
+name = "testing phase";
+console.log(address, zip, state);
+console.log(name);
